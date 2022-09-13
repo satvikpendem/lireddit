8d9734b02e322336e8552735a94875f60011c256
Satvik Pendem <satvik.pendem@gmail.com>
2022-09-13 00:29:24 -0400

    Change mikro-orm to only use js / dist

--- backend/src/mikro-orm.config.ts
+++ backend/src/mikro-orm.config.ts
@@ -3,17 +3,17 @@ import { PostgreSqlDriver } from "@mikro-orm/postgresql";
 import { __prod__ } from "./constants";
 
 export default {
-	dbName: "lireddit",
-	type: "postgresql",
-	debug: !__prod__,
-	user: "api_user",
-	password: "postgres",
-	entities: ["./dist/entities"],
-	entitiesTs: ["./src/entities"],
-	allowGlobalContext: true,
-	migrations: {
-		path: "./src/migrations",
-		pattern: /^[\w-]+\d+\.js$/,
-		disableForeignKeys: false,
-	},
+  dbName: "lireddit",
+  type: "postgresql",
+  debug: !__prod__,
+  user: "api_user",
+  password: "postgres",
+  entities: ["./dist/entities"],
+  entitiesTs: ["./src/entities"],
+  allowGlobalContext: true,
+  migrations: {
+    path: "./dist/migrations",
+    pattern: /^[\w-]+\d+\.js$/,
+    disableForeignKeys: false,
+  },
 } as Options<PostgreSqlDriver> | Configuration<PostgreSqlDriver> | undefined;

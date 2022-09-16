4186f6116476b56466d6a32eb39ba604f68f1f1b
Satvik Pendem <satvik.pendem@gmail.com>
2022-09-15 21:04:37 -0400

    Remove User and Comment from schema, for now, to test Post only

--- backend/src/schema.ts
+++ backend/src/schema.ts
@@ -1,35 +1,157 @@
 import { builder } from "./builder";
 import { db } from "./db";
 
-builder.prismaObject("User", {
-	fields: (t) => ({
-		id: t.exposeID("id"),
-		firstName: t.exposeString("firstName"),
-		lastName: t.exposeString("lastName"),
-		fullName: t.string({
-			resolve: (user) => `${user.firstName} ${user.lastName}`,
-		}),
-		posts: t.relation("posts"),
-		comments: t.relation("comments"),
-	}),
-});
+// builder.prismaObject("User", {
+// 	fields: (t) => ({
+// 		id: t.exposeID("id"),
+// 		firstName: t.exposeString("firstName"),
+// 		lastName: t.exposeString("lastName"),
+// 		fullName: t.string({
+// 			resolve: (user) => `${user.firstName} ${user.lastName}`,
+// 		}),
+// 		posts: t.relation("posts"),
+// 		comments: t.relation("comments"),
+// 	}),
+// });
+
+// builder.prismaObject("Post", {
+// 	fields: (t) => ({
+// 		id: t.exposeID("id"),
+// 		title: t.exposeString("title"),
+// 		content: t.exposeString("content"),
+// 		author: t.relation("author"),
+// 		comments: t.relation("comments"),
+// 	}),
+// });
+
+// builder.prismaObject("Comment", {
+// 	fields: (t) => ({
+// 		id: t.exposeID("id"),
+// 		comment: t.exposeString("comment"),
+// 		author: t.relation("author"),
+// 		post: t.relation("post"),
+// 	}),
+// });
+
+// builder.queryType({
+// 	fields: (t) => ({
+// 		posts: t.prismaField({
+// 			type: ["Post"],
+// 			nullable: true,
+// 			args: {
+// 				take: t.arg.int(),
+// 				skip: t.arg.int(),
+// 			},
+// 			resolve: (query, _, args) =>
+// 				db.post.findMany({
+// 					...query,
+// 					take: args.take ?? 10,
+// 					skip: args.skip ?? 0,
+// 				}),
+// 		}),
+// 		post: t.prismaField({
+// 			type: "Post",
+// 			nullable: true,
+// 			args: {
+// 				id: t.arg.id({ required: true }),
+// 			},
+// 			resolve: (query, _, args) =>
+// 				db.post.findUnique({
+// 					...query,
+// 					where: {
+// 						id: Number.parseInt(String(args.id)),
+// 					},
+// 				}),
+// 		}),
+// 		users: t.prismaField({
+// 			type: ["User"],
+// 			nullable: true,
+// 			args: {
+// 				take: t.arg.int(),
+// 				skip: t.arg.int(),
+// 			},
+// 			resolve: (query, _, args) =>
+// 				db.user.findMany({
+// 					...query,
+// 					take: args.take ?? 10,
+// 					skip: args.skip ?? 0,
+// 				}),
+// 		}),
+// 		user: t.prismaField({
+// 			type: "User",
+// 			nullable: true,
+// 			args: {
+// 				id: t.arg.id({ required: true }),
+// 			},
+// 			resolve: (query, _, args) =>
+// 				db.user.findUnique({
+// 					...query,
+// 					where: {
+// 						id: Number.parseInt(String(args.id)),
+// 					},
+// 				}),
+// 		}),
+// 		comments: t.prismaField({
+// 			type: ["Comment"],
+// 			nullable: true,
+// 			args: {
+// 				take: t.arg.int(),
+// 				skip: t.arg.int(),
+// 			},
+// 			resolve: (query, _, args) =>
+// 				db.comment.findMany({
+// 					...query,
+// 					take: args.take ?? 10,
+// 					skip: args.skip ?? 0,
+// 				}),
+// 		}),
+// 		comment: t.prismaField({
+// 			type: "Comment",
+// 			nullable: true,
+// 			args: {
+// 				id: t.arg.id({ required: true }),
+// 			},
+// 			resolve: (query, _, args) =>
+// 				db.comment.findUnique({
+// 					...query,
+// 					where: {
+// 						id: Number.parseInt(String(args.id)),
+// 					},
+// 				}),
+// 		}),
+// 	}),
+// });
+
+// builder.mutationType({
+// 	fields: (t) => ({
+// 		createUser: t.prismaField({
+// 			type: "User",
+// 			nullable: true,
+// 			args: {
+// 				firstName: t.arg.string({ required: true }),
+// 				lastName: t.arg.string({ required: true }),
+// 			},
+// 			resolve: (_, __, { firstName, lastName }) =>
+// 				db.user.create({
+// 					data: {
+// 						firstName,
+// 						lastName,
+// 					},
+// 				}),
+// 		}),
+// 	}),
+// });
 
 builder.prismaObject("Post", {
 	fields: (t) => ({
 		id: t.exposeID("id"),
 		title: t.exposeString("title"),
-		content: t.exposeString("content"),
-		author: t.relation("author"),
-		comments: t.relation("comments"),
-	}),
-});
-
-builder.prismaObject("Comment", {
-	fields: (t) => ({
-		id: t.exposeID("id"),
-		comment: t.exposeString("comment"),
-		author: t.relation("author"),
-		post: t.relation("post"),
+		createdAt: t.string({
+			resolve: (parent) => parent.createdAt.toISOString(),
+		}),
+		updatedAt: t.string({
+			resolve: (parent) => parent.updatedAt.toISOString(),
+		}),
 	}),
 });
 
@@ -63,59 +185,51 @@ builder.queryType({
 					},
 				}),
 		}),
-		users: t.prismaField({
-			type: ["User"],
+	}),
+});
+
+builder.mutationType({
+	fields: (t) => ({
+		createPost: t.prismaField({
+			type: "Post",
 			nullable: true,
 			args: {
-				take: t.arg.int(),
-				skip: t.arg.int(),
+				title: t.arg.string({ required: true }),
 			},
-			resolve: (query, _, args) =>
-				db.user.findMany({
-					...query,
-					take: args.take ?? 10,
-					skip: args.skip ?? 0,
-				}),
-		}),
-		user: t.prismaField({
-			type: "User",
-			nullable: true,
-			args: {
-				id: t.arg.id({ required: true }),
-			},
-			resolve: (query, _, args) =>
-				db.user.findUnique({
-					...query,
-					where: {
-						id: Number.parseInt(String(args.id)),
+			resolve: (_, __, { title }) =>
+				db.post.create({
+					data: {
+						title,
 					},
 				}),
 		}),
-		comments: t.prismaField({
-			type: ["Comment"],
+		updatePost: t.prismaField({
+			type: "Post",
 			nullable: true,
 			args: {
-				take: t.arg.int(),
-				skip: t.arg.int(),
+				id: t.arg.id({ required: true }),
+				title: t.arg.string({ required: true }),
 			},
-			resolve: (query, _, args) =>
-				db.comment.findMany({
-					...query,
-					take: args.take ?? 10,
-					skip: args.skip ?? 0,
+			resolve: (_, __, { id, title }) =>
+				db.post.update({
+					where: {
+						id: Number.parseInt(String(id)),
+					},
+					data: {
+						title,
+					},
 				}),
 		}),
-		comment: t.prismaField({
-			type: "Comment",
+		deletePost: t.prismaField({
+			type: "Post",
 			nullable: true,
 			args: {
 				id: t.arg.id({ required: true }),
 			},
-			resolve: (query, _, args) =>
-				db.comment.findUnique({
-					...query,
+			resolve: (_, __, { id }) =>
+				db.post.delete({
 					where: {
-						id: Number.parseInt(String(args.id)),
+						id: Number.parseInt(String(id)),
 					},
 				}),
 		}),

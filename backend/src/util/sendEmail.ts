82b556ca2acfed355774f7cfee22afec425a5ae4
Satvik Pendem <satvik.pendem@gmail.com>
2022-09-27 00:35:42 -0400

    Refactor sendEmail slightly

--- backend/src/util/sendEmail.ts
+++ backend/src/util/sendEmail.ts
@@ -4,17 +4,14 @@ import nodemailer, { SendMailOptions } from "nodemailer";
 export const sendEmail = async (options: SendMailOptions) => {
   // Generate test SMTP service account from ethereal.email
   // Only needed if you don't have a real mail account for testing
-  let testAccount = await nodemailer.createTestAccount();
+  let { user, pass } = await nodemailer.createTestAccount();
 
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
     host: "smtp.ethereal.email",
     port: 587,
     secure: false, // true for 465, false for other ports
-    auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass, // generated ethereal password
-    },
+    auth: { user, pass },
   });
 
   // send mail with defined transport object

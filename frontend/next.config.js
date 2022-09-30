43e107717262b1564c61cee931e6349f04689a45
Satvik Pendem <satvik.pendem@gmail.com>
2022-09-30 19:53:29 -0400

    Add frontend dockerfile

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,11 @@
-const {
-  createVanillaExtractPlugin
-} = require('@vanilla-extract/next-plugin');
-const withVanillaExtract = createVanillaExtractPlugin();
+const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin")
+const withVanillaExtract = createVanillaExtractPlugin()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: "standalone",
 }
 
 module.exports = withVanillaExtract(nextConfig)
